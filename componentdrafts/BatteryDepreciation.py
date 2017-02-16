b3953ae87814a49d27a266685ba18f9d8c021810
Mike Roup <michael.roup@pnnl.gov>
2017-02-15 17:22:00 -0800

    cleanup batter component draft

--- componentdrafts/BatteryDepreciation.py
+++ componentdrafts/BatteryDepreciation.py
@@ -1,4 +1,3 @@
-
 # coding: utf-8
 
 # In[ ]:
@@ -9,41 +8,43 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-BatteryCost=15000  #replacement battery cost in $
+BatteryCost = 15000  #replacement battery cost in $
 DODCurve = pd.read_csv('C:\Users\d3x836\Desktop\PNNL-Nick Fernandez\GMLC\BatteryDODcurve.csv', header=0)
 SOCForecast = pd.read_csv('C:\Users\d3x836\Desktop\PNNL-Nick Fernandez\GMLC\Battery24hrSOCforecast.csv', header=0)
-DOD= DODCurve['DOD'].values
-Cycles=DODCurve['Cycles'].values
+DOD = DODCurve['DOD'].values
+Cycles = DODCurve['Cycles'].values
 SOC = SOCForecast['SOC_Forecast'].values
 Hour = SOCForecast['Hour'].values
-Depreciation =0
+Depreciation = 0
 
-Rows=len(SOC)
+Rows = len(SOC)
 
 PrevSOC = []
 CurrSOC = []
-for i in range(0,Rows+1):
-    if i>1:
+
+for i in range(0, Rows+1):
+    if i > 1:
         PrevSOC.append(SOC[i-2])
         CurrSOC.append(SOC[i-1])
 
-for i in range(0,Rows-1): 
-    if i==0:
-        Peak=PrevSOC[i]
-        Valley=PrevSOC[i]
-    if CurrSOC[i]<PrevSOC[i]:
-        Valley=CurrSOC[i]
-    if CurrSOC[i]>PrevSOC[i] and PrevSOC[i]==Valley and i>1:
-        CurrDOD=Peak-Valley
+for i in range(0, Rows-1):
+    if i == 0:
+        Peak = PrevSOC[i]
+        Valley = PrevSOC[i]
+
+    if CurrSOC[i] < PrevSOC[i]:
+        Valley = CurrSOC[i]
+
+    if CurrSOC[i] > PrevSOC[i] and PrevSOC[i] == Valley and i > 1:
+        CurrDOD = Peak - Valley
         print CurrDOD
-        CurrCycles= np.interp(CurrDOD, DOD, Cycles)
+        CurrCycles = np.interp(CurrDOD, DOD, Cycles)
         print CurrCycles
-        CurrDepreciation =  1/CurrCycles*BatteryCost
+        CurrDepreciation = 1 / CurrCycles * BatteryCost
         print CurrDepreciation
-        Depreciation=Depreciation+CurrDepreciation
-    if CurrSOC[i]>PrevSOC[i]:
-        Peak=CurrSOC[i]
+        Depreciation = Depreciation + CurrDepreciation
+    if CurrSOC[i] > PrevSOC[i]:
+        Peak = CurrSOC[i]
 
 
 print Depreciation
-
