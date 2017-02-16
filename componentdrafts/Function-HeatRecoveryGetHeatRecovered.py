47560c471a5972b58b9b11d7550fe883ee396c4e
Mike Roup <michael.roup@pnnl.gov>
2017-02-16 14:04:04 -0800

    cleanup heat recovery component draft

--- componentdrafts/Function-HeatRecoveryGetHeatRecovered.py
+++ componentdrafts/Function-HeatRecoveryGetHeatRecovered.py
@@ -1,18 +1,14 @@
-
 # coding: utf-8
 
-# In[ ]:
-
-def GetHeatRecovered(HotWaterLoopFluid,T_water_i,TWaterOut,MFR_water):   
+def GetHeatRecovered(HotWaterLoopFluid,T_water_i,TWaterOut,MFR_water):
     if HotWaterLoopFluid == 'Water':
-        cp_water=4186 #j/kg-K    
+        cp_water = 4186 #j/kg-K
     elif HotWaterLoopFluid == 'GlycolWater30':
-        cp_water=3913 #j/kg-K
+        cp_water = 3913 #j/kg-K
     elif HotWaterLoopFluid == 'GlycolWater50':
-        cp_water=3558 #j/kg-K
+        cp_water = 3558 #j/kg-K
     else:
-        cp_water=4186 #j/kg-K
-    HeatRecovered = MFR_water*cp_water*(TWaterOut-T_water_i)/1000 #kW
-    return HeatRecovered                     
-                      
+        cp_water = 4186 #j/kg-K
 
+    HeatRecovered = MFR_water * cp_water * (TWaterOut - T_water_i) / 1000 #kW
+    return HeatRecovered
