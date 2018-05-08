4adae48ca3d1b08e8927b6e93ddf9102b2fe492b
Kyle Monson <kyle.monson@pnnl.gov>
2018-05-08 15:56:15 -0700

    Update command line tests.

--- test-year.sh
+++ test-year.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-python main.py scenario_c.config --input-data "battery_data" --output-data "command_output.csv"
+python main.py scenario_c/config --input-data "battery_data" --output-data "command_output.csv"
